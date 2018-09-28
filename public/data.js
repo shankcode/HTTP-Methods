0264708df314ff3d616b504ca99e0381443842ec
Shashank Yadav <shashank.5060@gmail.com>
2018-09-28 20:55:58 +0530

    JSON Data update

--- public/data.js
+++ public/data.js
@@ -1,47 +1,115 @@
 var friends_list = {
-  "sashi": {
-    "id": 1,
-    "occupation": "Business"
+  "boys": {
+      1: {
+        "name": "Sashi",
+        "lastname": "Chaubey",
+        "other_data": {
+          "occupation": "Business",
+          "place": "Bihar",
+          "contact": [999, 888, 999, 0]
+        }
+      },
+      2: {
+        "name": "Nishant",
+        "lastname": "Shah",
+        "other_data": {
+          "occupation": "Business",
+          "place": "Jharkhand",
+          "contact": [997, 889, 999, 4]
+        }
+      },
+      3: {
+        "name": "Sandeep",
+        "lastname": "Kushwaha",
+        "other_data": {
+          "occupation": "Developer",
+          "place": "Varanasi, Chennai",
+          "contact": [978, 892, 933, 6]
+        }
+      },
+      4: {
+        "name": "Saurav",
+        "lastname": "Kumar",
+        "other_data": {
+          "occupation": "Banker",
+          "place": "Bihar, Hyderabad",
+          "contact": [923, 728, 339, 2]
+        }
+      },
+      5: {
+        "name": "Nitish",
+        "lastname": "Srivastava",
+        "other_data": {
+          "occupation": "Software Testing",
+          "place": "Bihar, Hyderabad",
+          "contact": [789, 678, 489, 1]
+        }
+      },
+      6: {
+        "name": "Mohit",
+        "lastname": "Gauda",
+        "other_data": {
+          "occupation": "UI/UX Designer",
+          "place": "Bhilai, Chandigarh",
+          "contact": [923, 988, 979, 3]
+        }
+      },
+      7: {
+        "name": "Ritesh",
+        "lastname": "Kumar",
+        "other_data": {
+          "occupation": "Application Analyst",
+          "place": "Bihar, Coimbator",
+          "contact": [967, 348, 569, 0]
+        }
+      },
+      8: {
+        "name": "Shashank",
+        "lastname": "Yadav",
+        "other_data": {
+          "occupation": "complicated!!!",
+          "place": "Uttar Pradesh, New Delhi",
+          "contact": [459, 348, 769, 0]
+        }
+      },
+      9: {
+        "name": "Puranjan",
+        "lastname": "Das",
+        "other_data": {
+          "occupation": "Not Known",
+          "place": "New Delhi",
+          "contact": [239, 348, 549, 0]
+        }
+      }
   },
-  "nishant": {
-    "id": 2,
-    "occupation": "Business"
-  },
-  "sandeep": {
-    "id": 3,
-    "occupation": "Developer"
-  },
-  "saurav": {
-    "id": 4,
-    "occupation": "Banker"
-  },
-  "nitish": {
-    "id": 5,
-    "occupation": "Software Testing"
-  },
-  "mohit": {
-    "id": 6,
-    "occupation": "UI/UX Designer"
-  },
-  "ritesh": {
-    "id": 7,
-    "occupation": "Application Analyst"
-  },
-  "ratnadeepa": {
-    "id": 8,
-    "occupation": "Application Analyst"
-  },
-  "pooja": {
-    "id": 9,
-    "occupation": "UI/UX Designer"
-  },
-  "shaheen": {
-    "id": 10,
-    "occupation": "Designer"
-  },
-  "shashank": {
-    "id": 11,
-    "occupation": "complicated!!!"
+"girls": {
+    10: {
+      "name": "Ratna",
+      "lastname": "Gupta",
+      "other_data": {
+        "occupation": "Application Analyst",
+        "place": "Assam, Hyderabad",
+        "contact": [349, 458, 129, 8]
+      }
+    },
+    11: {
+      "name": "Pooja",
+      "lastname": "Jain",
+      "other_data": {
+        "occupation": "UI/UX Designer",
+        "place": "Assam",
+        "contact": [339, 548, 969, 1]
+      }
+    },
+    12: {
+      "name": "Shaheen",
+      "lastname": "Salam",
+      "other_data": {
+        "occupation": "Designer",
+        "place": "Port Blair",
+        "contact": [129, 678, 459, 3]
+      }
+    }
   }
 }
 
