9060ba00d2dc416977ac26ec528a938f57eefcee
Shashank Yadav <shashank.5060@gmail.com>
2018-09-28 20:22:45 +0530

    GET Routes modified

--- app.js
+++ app.js
@@ -6,23 +6,56 @@ var friends_list = require("./public/data");
 
 var PORT = process.env.PORT || 4001;
 
-
+//-------------------MIDDLEWARES----------------------
 app.use(express.static("public"));
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
 
-app.get("/friends", (req, res, next) => {
-  res.send(friends_list);
-})
-
-app.get("/friends/:name", (req, res, next) => {
-  var name = req.params.name;
-  if(!friends_list[name]) {
+app.param("id", (req, res, next, id) => {
+  var id = id;
+  if (Object.keys(friends_list.boys).includes(id)) {
+    for (var key in friends_list.boys) {
+      if (key === id) {
+        req.show = friends_list.boys[key];
+        next();
+      }
+    }
+  } else if (Object.keys(friends_list.girls).includes(id)) {
+    for (var key in friends_list.girls) {
+      if (key === id) {
+        req.show = friends_list.girls[key];
+        next();
+      }
+    }
+  } else {
     return res.status(404).send();
   }
-  res.send(friends_list[name]);
+});
+
+//---------------------ROUTES---------------------------
+app.get("/friends/", (req, res, next) => {
+  res.send(friends_list);
+});
+
+app.get("/friends/:id", (req, res) => {
+      res.send(req.show);
+});
+
+app.post('/friends/:id', (req, res, next) => {
+  var newId = req.query;
+  console.log(newId);
+});
+
+app.put('/friends/:id', (req, res, next) => {
+
+});
+
+app.delete('friends/:id', (req, res, next) => {
+  req.show = null;
+  res.send(req.show);
 })
 
-
+//-----------------SERVER PORT INIT---------------------
 app.listen(PORT, () => {
   console.log("Server is listening...")
 })
